b7f2edaed29d16212cb307a8bbbad582dfd59050
balkrishan <balkrishan@maansoftwares.com>
2017-05-04 19:58:15 +0530

    update

--- app/app.component.spec.ts
+++
@@ -1,6 +0,0 @@
-import { AppComponent } from './app.component';
-
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
